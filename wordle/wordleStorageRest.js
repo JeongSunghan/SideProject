af765349d83ad9d654f584bd6b3a1a138e2ecaed
sunghan <jsh000311@gmail.com>
2024-09-24 04:51:40 +0900

    update
    1. 로컬스트리지 사용해서 기록 저장
    -> 애먹은 부분 : 자꾸 정답을 맞춰도 제출 버튼을 눌러야만 정답을 측정하길래 이전기록을 비교해서 정답일 경우 자동으로 게임이 종료되게 함수와 로직 변경..ㅠ

---
+++ wordle/wordleStorageRest.js
@@ -0,0 +1,16 @@
+// 초기화 버튼 클릭 시 실행되는 함수
+document.querySelector("#clearDataBtn").addEventListener("click", function () {
+    // 비밀번호 입력창 표시
+    const password = prompt("비밀번호를 입력하세요:");
+  
+    // 비밀번호가 2683과 일치하는지 확인
+    if (password === "2683") {
+      // 로컬 스토리지에서 기록 삭제
+      localStorage.removeItem("results");
+      alert("기록이 초기화되었습니다.");
+      displayLeaderboard(); // 리더보드 갱신
+    } else {
+      alert("비밀번호가 틀렸습니다. 기록을 초기화할 수 없습니다.");
+    }
+  });
+  
