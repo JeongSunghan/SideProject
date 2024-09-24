fb507f4fb8c11288339348553b8e507865f560d2
sunghan <jsh000311@gmail.com>
2024-09-25 04:24:37 +0900

    wordle - finish
    첫 사이드 프로젝트 완성
    후기 : 아직 많이 미흡하다..
    
    나중에 좀 더 리팩토링을 해야할 거 같다.

--- wordle/js/wordleStorageRest.js
+++ wordle/js/wordleStorageRest.js
@@ -1,12 +1,11 @@
 // 초기화 버튼 클릭 시 실행되는 함수
-document.querySelector("#clearDataBtn").addEventListener("click", function () {
-    // 비밀번호 입력창 표시
+document.querySelector("#clearDataBtn").addEventListener("click", function () {   
     const password = prompt("비밀번호를 입력하세요:");
   
     if (password === "2683") {
       localStorage.removeItem("results");
       alert("기록이 초기화되었습니다.");
-      displayLeaderboard(); // 리더보드 갱신
+      displayLeaderboard(); 
     } else {
       alert("비밀번호가 틀렸습니다. 기록을 초기화할 수 없습니다.");
     }
